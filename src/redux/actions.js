9ec6cbfc1d2471facb63d34782461bd93384e5d8
Sian Richards <sian_richards@hotmail.co.uk>
2023-10-11 18:08:17 +0100

    prettify redux files

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,102 +1,115 @@
-import { INCREMENT, DECREMENT, RESET, USER_IS_LOGGED_IN, USER_IS_LOGGED_OUT, SET_USER, UPDATE_MODAL , DELETE_BLOG_POST, CANCEL_DELETE_BLOG_POST, SEND_POST_ID, UPDATE_POST, CANCEL_UPDATE_POST } from './types';
+import {
+  INCREMENT,
+  DECREMENT,
+  RESET,
+  USER_IS_LOGGED_IN,
+  USER_IS_LOGGED_OUT,
+  SET_USER,
+  UPDATE_MODAL,
+  DELETE_BLOG_POST,
+  CANCEL_DELETE_BLOG_POST,
+  SEND_POST_ID,
+  UPDATE_POST,
+  CANCEL_UPDATE_POST,
+} from "./types";
 
 export const increment = () => {
-    return {
-        type: INCREMENT
-    }
+  return {
+    type: INCREMENT,
+  };
 };
 
 export const decrement = () => {
-    return {
-        type: DECREMENT
-    }
+  return {
+    type: DECREMENT,
+  };
 };
 
 export const reset = () => {
-    return {
-        type: RESET
-    }
+  return {
+    type: RESET,
+  };
 };
 
 export const userIsLoggedIn = () => {
-    return {
-        type: USER_IS_LOGGED_IN
-    }
+  return {
+    type: USER_IS_LOGGED_IN,
+  };
 };
 
 export const userIsLoggedOut = () => {
-    return {
-        type: USER_IS_LOGGED_OUT
-    }
+  return {
+    type: USER_IS_LOGGED_OUT,
+  };
 };
 
-export const setUserAction = user => {
-    return {
-        type: SET_USER,
-        payload: user
-    }
+export const setUserAction = (user) => {
+  return {
+    type: SET_USER,
+    payload: user,
+  };
 };
 
-export const modalInfoAction = modalInfo => {
-    return {
-        type: UPDATE_MODAL,
-        payload: modalInfo
-    }
+export const modalInfoAction = (modalInfo) => {
+  return {
+    type: UPDATE_MODAL,
+    payload: modalInfo,
+  };
 };
 
 export const deleteBlogPostAction = () => {
-    return {
-        type: DELETE_BLOG_POST,
-    }
+  return {
+    type: DELETE_BLOG_POST,
+  };
 };
 
 export const cancelDeleteBlogPostAction = () => {
-    return {
-        type: CANCEL_DELETE_BLOG_POST
-    }
+  return {
+    type: CANCEL_DELETE_BLOG_POST,
+  };
 };
 
 export const sendBlogPostIdAction = (blogPostId) => {
-    return {
-        type: SEND_POST_ID,
-        payload: blogPostId
-    }
+  return {
+    type: SEND_POST_ID,
+    payload: blogPostId,
+  };
 };
 
 export const updateBlogPostAction = () => {
-    return {
-        type: UPDATE_POST,
-    }
+  return {
+    type: UPDATE_POST,
+  };
 };
 
 export const cancelUpdateBlogPostAction = () => {
-    return {
-        type: CANCEL_UPDATE_POST,
-    }
+  return {
+    type: CANCEL_UPDATE_POST,
+  };
 };
 
 export const editProfileAction = (profileText) => {
-    return {
-        type: 'EDIT_PROFILE',
-        payload: profileText
-    }
+  return {
+    type: "EDIT_PROFILE",
+    payload: profileText,
+  };
 };
 
 export const showProfileEditModeAction = () => {
-    return {
-        type: 'SHOW_PROFILE_EDIT_MODE'
-    }
+  return {
+    type: "SHOW_PROFILE_EDIT_MODE",
+  };
 };
 
 export const showProfileFinalModeAction = () => {
-    return {
-        type: 'SHOW_PROFILE_FINAL_MODE'
-    }
+  return {
+    type: "SHOW_PROFILE_FINAL_MODE",
+  };
 };
 
-export const sendStorageRefAction = storageRef => {
-    return {
-        type: 'SEND_STORAGE_REF',
-        payload: storageRef
-    }
-}
+export const sendStorageRefAction = (storageRef) => {
+  return {
+    type: "SEND_STORAGE_REF",
+    payload: storageRef,
+  };
+};
